29c420b6818e9e9667c9b17de20c40b937fb79f8
ACAYIRAY <aysegulcayiraydar@gmail.com>
2021-07-04 13:18:41 +0200

    added update and result guest html classes!

--- src/main/java/com/restaurant/reservationApp/RestaurantAppApplication.java
+++ src/main/java/com/restaurant/reservationApp/RestaurantAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestaurantAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RestaurantAppApplication.class, args);
 	}
 
