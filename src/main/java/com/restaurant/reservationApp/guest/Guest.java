8a59c38c0c60b356a4e3a4130749c7afc2edce3d
ACAYIRAY <aysegulcayiraydar@gmail.com>
2021-07-12 15:10:07 +0200

    added Guest.html different table format!

--- src/main/java/com/restaurant/reservationApp/guest/Guest.java
+++ src/main/java/com/restaurant/reservationApp/guest/Guest.java
@@ -27,7 +27,7 @@ public class Guest {
     }
 
     public String getFirstName() {
-        return firstName;
+        System.out.println();return firstName;
     }
 
     public void setFirstName(String firstName) {
