f08f97ccd3fce7e1676205ec5e269d6b66281b3f
AKOCAK <ayse.kocak@capgemini.com>
2021-08-09 22:58:57 +0200

    login page designed and finished

--- src/main/java/com/restaurant/reservationApp/suplement/SupplementController.java
+++ src/main/java/com/restaurant/reservationApp/suplement/SupplementController.java
@@ -1,10 +1,12 @@
 package com.restaurant.reservationApp.suplement;
 
 
+import com.restaurant.reservationApp.employee.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,16 @@ public class SupplementController {
     public ResponseEntity<List<Supplement>> getAllSupplements() {
         return new ResponseEntity<>(supplementService.getAllSupplements(), HttpStatus.OK);
 
+    }
+
+    @GetMapping("/supplement/{id}")
+    public ResponseEntity<Supplement> getSupplementById(@PathVariable(name = "id", required = true) long id) {
+        Supplement supplement = supplementService.getSupplementById(id);
+        return new ResponseEntity<>(supplement, HttpStatus.OK);
+    }
+    @GetMapping("supplement/{name}")
+    public ResponseEntity<Supplement> getSupplementByName(@PathVariable(name = "name", required = true) String name){
+        Supplement supplement = SupplementService.getSupplementByName(name);
+        return new ResponseEntity<>(supplement, HttpStatus.OK);
     }
 }
--- src/main/java/com/restaurant/reservationApp/suplement/SupplementService.java
+++ src/main/java/com/restaurant/reservationApp/suplement/SupplementService.java
@@ -3,5 +3,11 @@ package com.restaurant.reservationApp.suplement;
 import java.util.List;
 
 public interface SupplementService {
+    static Supplement getSupplementByName(String name) {
+        return null;
+    }
+
     List<Supplement> getAllSupplements();
+
+    public Supplement getSupplementById(long id);
 }
